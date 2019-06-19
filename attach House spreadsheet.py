78ef62bd7df5c178bd340d070cfae7866fce52d6
Erich Rentz <rentz.erich@gmail.com>
2019-06-18 21:55:51 -0400

    usability changes
    set the correct env.workspace, removed user specific mapping, cleaned up erroneous geoprocessing calls, removed superfluous steps, resused as much source data as possible, added getcwd() call for if the script is moved into the "Source" folder

--- attach House spreadsheet.py
+++ attach House spreadsheet.py
@@ -1,22 +1,26 @@
 # -*- coding: utf-8 -*-
 
 import arcpy
+import os
 from arcpy import env
 
-env.workspace           =   r''
-env.overwriteOutput     =   True
-in_csv                  =   r'C:\USS\United States Solar Corporation\Site Selection - Documents\Data\State\MN\Source\shp_bdry_housedistricts2012\HouseMembers.csv'
-in_shp                  =   r'C:\USS\United States Solar Corporation\Site Selection - Documents\Data\State\MN\Source\shp_bdry_housedistricts2012\hse2012_vtd2016.shp'
-out_table               =   r'C:\Users\JacobNordberg\Documents\ArcGIS\Default.gdb'
-out_shp                 =   "Updated_House"
-phi                     =   "DeleteTemp"
+env.overwriteOutput     =   True 
+#cwd = os.getcwd()!
+env.workspace = r'C:\USS\United States Solar Corporation\Site Selection - Documents\Data\State\MN\Source\shp_bdry_housedistricts2012' #=cwd
+
+in_csv                  =   'HouseMembers.csv'
+in_shp                  =   'hse2012_vtd2016.shp'
+out_shp                 =   'MN_House_2019.shp'
+phi                     =   "DeleteTemp"      
         
-        
-arcpy.TableToTable_conversion(in_csv, out_table, "HouseUpdate" + phi)
-arcpy.CopyFeatures_management(in_shp, "NMCongressional"+phi)
-join_fields = ['House_Member', 'District', 'Party', 'Office_Address', 'City_State_Zip', 'Phone_Number', 'Email']
-arcpy.JoinField_management("NMCongressional"+phi, "MNLEGDIST", "HouseUpdate"+ phi, "District", join_fields)
-arcpy.CopyFeatures_management("NMCongressional"+phi, out_shp)
-arcpy.DeleteField_management(out_shp, ["district", "party", "name"])
-arcpy.Delete_management("NMCongressional"+phi)
-arcpy.Delete_management("HouseUpdate" + phi)
+arcpy.TableToTable_conversion(in_csv, "in_memory", phi)
+arcpy.CopyFeatures_management(in_shp, out_shp)
+
+join_fields = ['House_Member', 'Party']
+arcpy.JoinField_management(out_shp, "MNLEGDIST", os.path.join("in_memory", phi), "District", join_fields)
+
+arcpy.CalculateField_management(out_shp, 'name', '!House_Memb!', 'PYTHON_9.3') #<-- this would be easier if you picked better names in your scraping
+arcpy.CalculateField_management(out_shp, 'party', '!Party_1!', 'PYTHON_9.3') #<-- this would be easier if you picked better names in your scraping
+
+arcpy.DeleteField_management(out_shp, ["memid", "House_Memb", "Party_1"])
+arcpy.Delete_management(os.path.join("in_memory", phi))
